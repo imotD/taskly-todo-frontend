e3d254ab7af42faf2d0c05e7ee4ea40acdc55c06
Tommy Alhamra <tommy.alhamra@ist.id>
2025-02-16 14:37:28 +0700

    fix: structure folder

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as AddTodoForm } from './AddTodoForm'
+export { default as TodoList } from './TodoList'
+export { default as TodoItem } from './TodoItem'
