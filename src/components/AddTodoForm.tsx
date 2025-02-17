7aafdb0f03364209e21fbb8150ed84bcb2cde708
Tommy Alhamra <tommy.alhamra@ist.id>
2025-02-17 14:24:03 +0700

    init update

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,14 @@
+
+function AddTodoForm() {
+    // const [count, setCount] = useState(0)
+  
+    return (
+      <>
+       <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, adipisci!
+       </div>
+      </>
+    )
+  }
+  
+  export default AddTodoForm
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function TodoItem() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, adipisci!
+     </div>
+    </>
+  )
+}
+
+export default TodoItem
