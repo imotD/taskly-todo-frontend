c1c21c5a0f40939b87d8750bf0fa38bc7d3433a9
Tommy Alhamra <tommy.alhamra@ist.id>
2025-02-27 11:30:29 +0700

    feat: add handleMode darkmod and add handleInput

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,33 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 function TodoItem() {
-  // const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e: any) => {
+    setTodo(e.target.value);
+  };
+
+  const handleKeydown = (e: any) =>{
+    if(e.key === "Enter"){
+      e.preventDefault();
+      console.log(todo);
+    }
+  }
 
   return (
-    <div className="mt-15 mb-5">
-    <input className="w-full py-5 px-20 text-2xl bg-white rounded " id="todo" type="text" placeholder="create todo..."></input>
-   </div>
-  )
+    <div className="mt-15 mb-5 relative">
+      <div className="todo-input"></div>
+      <input
+        className="w-full py-5 px-20 text-2xl bg-white rounded "
+        id="todo"
+        type="text"
+        placeholder="create todo..."
+        onChange={handleChange}
+        onKeyDown={handleKeydown}
+        value={todo}
+      ></input>
+    </div>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
-// import { useState } from 'react'
-import Light from '../assets/light.svg'
+import { useState } from 'react'
 import { TodoItem, TodoList } from '../components';
-// import Dark from '../assets/dark.svg'
+
+import LightIcon from '../assets/light.svg'
+import DarkIcon from '../assets/dark.svg'
 
 function Home() {
-  // const [count, setCount] = useState(0)
+  const [mode, setMode] = useState('light');
+
+  const handleMode = () =>{
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+
+  }
 
   return (
     <>
@@ -16,7 +22,7 @@ function Home() {
         >
           <span>TODO</span>
 
-          <img src={Light} alt="icon" className="w-10 h-10 cursor-pointer" />
+          <img src={mode === 'light' ? LightIcon : DarkIcon} alt="icon" className="w-10 h-10 cursor-pointer" onClick={handleMode} />
         </div>
 
         <TodoItem />
@@ -26,4 +32,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Home
