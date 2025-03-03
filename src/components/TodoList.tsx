9541b7798a014868d2a4ff7aedc0491763ae8347
Tommy Alhamra <tommy.alhamra@ist.id>
2025-03-03 10:41:44 +0700

    fix: todolist style

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,27 +1,52 @@
 // import { useState } from 'react'
 import CheckIcon from "../assets/check.svg?react";
-// import CancelIcon from "../assets/cancel.svg";
+import CancelIcon from "../assets/cancel.svg?react";
 
 function TodoList() {
   // const [count, setCount] = useState(0)
 
-  return (
-    <div className="bg-slate-100 rounded py-1">
-      <div className="flex justify-between items-center px-5">
-        <div className="flex justify-between items-center cursor-pointer">
-          <div className="p-2">
-            <div className="todo-circle p-2">
-            <CheckIcon className="w-6 h-6 text-white stroke-2" />
-            </div>
-          </div>
+  const todos = [
+    {
+      id: 1,
+      title: "Test",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      title: "Test 2",
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: "Test 3",
+      isCompleted: false
+    }
+  ]
 
-          <div className="text-2xl w-full text-left py-5 p-5">Test</div>
+  return (
+    <div className="bg-slate-100 rounded">
+      {todos.map((todo) => (
+        <div className="flex justify-between items-center px-5 py-1 todo-list">
+          <div className="flex justify-between items-center cursor-pointer">
+            <div className="p-2">
+              <div className={`todo-circle p-2 ${todo.isCompleted ? "todo-circle__check" : "todo-circle__none"}`}>
+                {todo.isCompleted ? <CheckIcon className="w-6 h-6 text-white stroke-2" /> : ''}
+              </div>
+            </div>
+
+            <div className={`text-2xl w-full text-left py-5 p-5 ${todo.isCompleted ? 'line-through' : ''}`}>{todo.title}</div>
+          </div>
+          {/* icon cancel */}
+          {
+            !todo.isCompleted ?
+              <div className="cursor-pointer">
+                <CancelIcon className="w-10 h-10 text-gray-500" />
+              </div> : ''
+          }
+
         </div>
-        {/* icon cancel */}
-        <div className="cursor-pointer">
-          {/* <img src={Cancel} alt="icon" className="w-10 h-10 cursor-pointer" /> */}
-        </div>
-      </div>
+      ))}
+
     </div>
   );
 }
